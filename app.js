8a0283bc8c8cc0831f04fb36c66fa3dbe0ef7970
Vishal Sharma <vishalsharma28784@gmail.com>
2023-09-01 18:41:22 +0530

    Used Sequelize Library

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -21,4 +22,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then(result => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
