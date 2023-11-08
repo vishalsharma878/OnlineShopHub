bca80ab589452cf4ddaca654deb74c4b44f0c5ba
Vishal Sharma <vishalsharma28784@gmail.com>
2023-11-08 20:10:31 +0530

    Deleting Cart Items

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('User_Id')
+  User.findById('USER_ID')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
