7ff705294b52af6f618a4e753128e4db915a3a5f
Vishal Sharma <vishalsharma28784@gmail.com>
2023-11-09 15:30:41 +0530

    Created User Schema

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -19,14 +19,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('USER_ID')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('USER_ID')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -36,7 +36,18 @@ app.use(errorController.get404);
 mongoose
   .connect(process.env.MONGO_CLIENT)
   .then(result => {
-    console.log("Connected")
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Vishal',
+          email: 'vis@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch(err => {
